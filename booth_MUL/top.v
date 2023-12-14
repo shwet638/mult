1a74752300187dd5bea3ba107a0302da52039da8
shwet_dhameliya <74365233+shwet638@users.noreply.github.com>
2023-12-14 09:08:36 +0530

    Update top.v

--- booth_MUL/top.v
+++ booth_MUL/top.v
@@ -5,15 +5,15 @@
 module top(input signed [7:0] a,
 	input signed [7:0] b,
 	input clk,
-	output  reg signed [14:0] c);
+	   output  reg signed [15:0] c);
 
 wire signed [8:0] temp_a;
 wire signed [7:0] temp_b;
-wire signed [59:0] pp;
-reg signed [14:0] pp_1;
-reg signed [29:15] pp_2;
-reg signed [44:30] pp_3;
-reg signed [59:45] pp_4;
+	wire signed [63:0] pp;
+	reg signed [15:0] pp_1;
+	reg signed [31:16] pp_2;
+	reg signed [47:32] pp_3;
+	reg signed [63:48] pp_4;
 assign temp_a=a>=b?{a,1'b0}:{b,1'b0};
 assign temp_b=a>=b?b:a;
 // always @(posedge clk or negedge clk)begin
@@ -26,10 +26,10 @@ assign temp_b=a>=b?b:a;
 // assign c=w1+w2
 always@(posedge clk)
 begin
-	pp_1=pp[14:0];
-	pp_2=pp[29:15];
-	pp_3=pp[44:30];
-	pp_4=pp[59:45];
+	pp_1=pp[15:0];
+	pp_2=pp[31:16];
+	pp_3=pp[47:32];
+	pp_4=pp[63:48];
 	c=pp_1+pp_2+pp_3+pp_4;
 	
 end
@@ -39,7 +39,7 @@ generate
 	for (i=0;i<4;i=i+1)
 	begin 
 	
-	new b1(.clk(clk),.opr(temp_a[i*2 +:3]),.extend_one(i),.b(temp_b),.pp(pp[i*15+:15]));
+		new b1(.clk(clk),.opr(temp_a[i*2 +:3]),.extend_one(i),.b(temp_b),.pp(pp[i*16+:16]));
 
 end
 endgenerate
